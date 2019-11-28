01ea9a632817cf076f3774911b25289ef59a2842
isabel <isabel.garvilopez@soprasteria.com>
2019-11-28 15:03:20 +0100

    all pieces move and capture rival pieces

--- Piece.py
+++ Piece.py
@@ -1,3 +1,6 @@
+from abc import abstractmethod
+
+
 class Piece:
     def __init__(self, icon, color):
         self._icon = icon
@@ -11,6 +14,21 @@ class Piece:
     def icon(self):
         return self._icon
 
+    @abstractmethod
+    def available_positions(self, color, from_row=None, to_row=None, from_col=None, to_col=None):
+        """Check if a piece can make that move.
+        Args:
+            color: color of the piece
+            from_row: row from where the piece wants to move
+            to_row: row to where the piece wants to move
+            from_col: column from where the piece wants to move
+            to_col: column to where the piece wants to move
+
+        Returns:
+            Whether the piece can make the move.
+        """
+        ...
+
     def __repr__(self):
         return f"{self._icon}{self._color}"
 
@@ -19,42 +37,137 @@ class Pawn(Piece):
     def __init__(self, color):
         super().__init__("P", color)
 
-    def available_positions(self, color, from_row, to_row):
+    def available_positions(self, color, from_row=None, to_row=None, from_col=None, to_col=None) -> bool:
         if color == "b":
-            if to_row == (from_row+1):
+            if to_row == (from_row-1) and to_col == from_col:
                 return True
+            return False
+        else:
+            if to_row == (from_row+1) and to_col == from_col:
+                return True
+            return False
 
 
 class King(Piece):
     def __init__(self, color):
         super().__init__("K", color)
 
+    def available_positions(self, color, from_row=None, to_row=None,
+                            from_col=None, to_col=None) -> bool:
+        if (to_row  == from_row+1 or to_row == from_row-1) and (to_col == from_col+1 or to_col == from_col-1):
+            return True
+        elif to_row == from_row+1 or to_row == from_row-1 or to_col == from_col+1 or to_col == from_col-1:
+            return True
+        elif to_row == (from_row-1):
+            return True
+        else:
+            return False
+
 
 class Knight(Piece):
     def __init__(self, color):
         super().__init__("N", color)
+
+    def available_positions(self, color, from_row=None, to_row=None,
+                            from_col=None, to_col=None) -> bool:
+        if color == "b":
+            if to_row == (from_row-2) and (to_col == from_col+1 or to_col == from_col-1):
+                return True
+            return False
+        else:
+            if to_row == (from_row+2) and (to_col == from_col+1 or to_col == from_col-1):
+                return True
+            return False
 
 
 class Rook(Piece):
     def __init__(self, color):
         super().__init__("R", color)
 
+    def available_positions(self, color, from_row=None, to_row=None,
+                            from_col=None, to_col=None) -> bool:
+        if (to_row > from_row or to_row < from_row) and to_col == from_col:
+            return True
+        elif (to_col > from_col or to_col < from_col) and to_row == from_row:
+            return True
+        else:
+            return False
+
 
 class GoldGeneral(Piece):
     def __init__(self, color):
         super().__init__("G", color)
+
+    def available_positions(self, color, from_row=None, to_row=None,
+                            from_col=None, to_col=None) -> bool:
+        if color == "b":
+            if to_row == (from_row-1) and (to_col == from_col+1 or to_col == from_col-1):
+                return True
+            elif to_row == from_row-1 or to_row == from_row+1 or to_col == from_col+1 or to_col == from_col-1:
+                return True
+            else:
+                return False
+        else:
+            if to_row == (from_row+1) and (to_col == from_col+1 or to_col == from_col-1):
+                return True
+            elif to_row == from_row-1 or to_row == from_row+1 or to_col == from_col+1 or to_col == from_col-1:
+                return True
+            else:
+                return False
 
 
 class SilverGeneral(Piece):
     def __init__(self, color):
         super().__init__("S", color)
 
+    def available_positions(self, color, from_row=None, to_row=None,
+                            from_col=None, to_col=None) -> bool:
+        if color == "b":
+            if (to_row == from_row-1 or to_row == from_row+1) and (to_col == from_col+1 or to_col == from_col-1):
+                return True
+            elif to_row == from_row-1:
+                return True
+            else:
+                return False
+        else:
+            if (to_row == from_row - 1 or to_row == from_row + 1) and (
+                    to_col == from_col + 1 or to_col == from_col - 1):
+                return True
+            elif to_row == from_row+1:
+                return True
+            else:
+                return False
+
 
 class Lance(Piece):
     def __init__(self, color):
         super().__init__("L", color)
 
+    def available_positions(self, color, from_row=None, to_row=None,
+                            from_col=None, to_col=None) -> bool:
+        if color == "b":
+            if to_col == from_col and to_row < from_row:
+                return True
+            return False
+        else:
+            if to_col == from_col and to_row > from_row:
+                return True
+            return False
+
 
 class Bishop(Piece):
     def __init__(self, color):
         super().__init__("B", color)
+
+    def available_positions(self, color, from_row=None, to_row=None,
+                            from_col=None, to_col=None) -> bool:
+        if to_row > from_row and to_col > from_col:
+            return True
+        elif to_row < from_row and to_col > from_col:
+            return True
+        elif to_row < from_row and to_col < from_col:
+            return True
+        elif to_row > from_row and to_col < from_col:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class Board:
                        self._pawns("b"),
                        self._rook_bishop("b"),
                        self._king("b")]
-        self.white_captured = 0
-        self.black_captured = 0
+        self.white_captured = []
+        self.black_captured = []
 
     @staticmethod
     def _pawns(color):
@@ -45,52 +45,183 @@ class Board:
                 else:
                     print("  ", end=" ")
             print("")
+        print(" ")
         print(f"9: ")
         print(f"Captured: {self.black_captured}")
         print("============Black==========")
 
-    def check_piece(self, row, col):
-        piece = self._board[row][col]
-        if not piece:
-            print("There is no piece on this position")
-            exit()
-        return piece
+    def check_piece(self, move_from_row, move_from_col):
+        piece_selected = self._board[move_from_row][move_from_col]
+        if piece_selected is " ":
+            raise Exception("There is no piece on this position")
+        elif piece_selected.color is not turn:
+            raise Exception("Please choose a piece of your color")
+        return piece_selected
 
-    def check_available_moves(self, piece, to_row, to_col, from_row, from_col):
-        icon = piece.icon()
-        if(icon == "P"):
-        pass
+    def check_available_moves(self, piece_to_move, move_to_row, move_to_col,
+                              move_from_row, move_from_col):
+        icon = piece_to_move.icon
+        result = False
+        if icon == "P":
+            result = piece_to_move.available_positions(piece.color,
+                                                       to_row=move_to_row,
+                                                       from_row=move_from_row,
+                                                       to_col=move_to_col,
+                                                       from_col=move_from_col)
+        elif icon == "K":
+            result = piece_to_move.available_positions(piece.color,
+                                                       to_row=move_to_row,
+                                                       from_row=move_from_row,
+                                                       to_col=move_to_col,
+                                                       from_col=move_from_col)
+        elif icon == "N":
+            result = piece_to_move.available_positions(piece.color,
+                                                       to_row=move_to_row,
+                                                       from_row=move_from_row,
+                                                       to_col=move_to_col,
+                                                       from_col=move_from_col)
+        elif icon == "G":
+            result = piece_to_move.available_positions(piece.color,
+                                                       to_row=move_to_row,
+                                                       from_row=move_from_row,
+                                                       to_col=move_to_col,
+                                                       from_col=move_from_col)
+        elif icon == "S":
+            result = piece_to_move.available_positions(piece.color,
+                                                       to_row=move_to_row,
+                                                       from_row=move_from_row,
+                                                       to_col=move_to_col,
+                                                       from_col=move_from_col)
+        elif icon == "L":
+            if piece_to_move.color == "w":
+                for i in range(move_from_row+1, move_to_row+1):
+                    print(self._board[i][move_to_col])
+                    if self._board[i][move_to_col] is not " ":
+                        return result
+            else:
+                for i in range(move_to_row, move_from_row-1):
+                    print(self._board[i][move_to_col])
+                    if self._board[i][move_to_col] == " ":
+                        return result
 
-    def check_position(self, piece, row, col, from_row, from_col):
-        new_pos = self._board[row][col]
-        if new_pos is " ":
-            if self.check_available_moves(piece, row, col, from_row, from_col):
-                self._board[row][col] = piece
-                self._board[from_row][from_col] = " "
+            result = piece_to_move.available_positions(piece.color,
+                                                       to_row=move_to_row,
+                                                       from_row=move_from_row,
+                                                       to_col=move_to_col,
+                                                       from_col=move_from_col)
+        elif icon == "R":
+            if move_to_col is move_from_col:
+                if move_to_row > move_from_row:
+                    for i in range(move_from_row+1, move_to_row+1):
+                        if self._board[i][move_to_col] is not " ":
+                            if self._board[i][move_to_col].color is piece_to_move.color:
+                                return result
+                else:
+                    for i in range(move_from_row-1, move_to_row-1, -1):
+                        print(self._board[i][move_to_col])
+                        if self._board[i][move_to_col] is not " ":
+                            if self._board[i][move_to_col].color is piece_to_move.color:
+                                return result
+            elif move_to_row is move_from_row:
+                if move_to_col > move_from_col:
+                    for i in range(move_from_col+1, move_to_col+1):
+                        if self._board[move_to_row][i] is not " ":
+                            if self._board[move_to_row][i].color is piece_to_move.color:
+                                return result
+                else:
+                    for i in range(move_from_col-1, move_to_col-1):
+                        if self._board[move_to_row][i] is not " ":
+                            if self._board[move_to_row][i].color is piece_to_move.color:
+                                return result
+
+            result = piece_to_move.available_positions(piece.color,
+                                                       to_row=move_to_row,
+                                                       from_row=move_from_row,
+                                                       to_col=move_to_col,
+                                                       from_col=move_from_col)
+        elif icon == "B":
+            if to_row < from_row and to_col > from_col:
+                for i in range(move_from_col+1, move_to_col+1):
+                    for j in range(move_from_row+1, move_to_row+1):
+                        if self._board[j][i] is not " ":
+                            if self._board[j][i].color is piece_to_move.color:
+                                return result
+            elif to_row > from_row and to_col > from_col:
+                for i in range(move_from_col+1, move_to_col+1):
+                    for j in range(move_from_row-1, move_to_row-1, -1):
+                        if self._board[j][i] is not " ":
+                            if self._board[j][i].color is piece_to_move.color:
+                                return result
+            elif to_row > from_row and to_col < from_col:
+                for i in range(move_from_col-1, move_to_col-1, -1):
+                    for j in range(move_from_row+1, move_to_row+1):
+                        if self._board[j][i] is not " ":
+                            if self._board[j][i].color is piece_to_move.color:
+                                return result
+            elif to_row < from_row and to_col < from_col:
+                for i in range(move_from_col-1, move_to_col-1, -1):
+                    for j in range(move_from_row-1, move_to_row-1, -1):
+                        if self._board[j][i] is not " ":
+                            if self._board[j][i].color is piece_to_move.color:
+                                return result
+
+            result = piece_to_move.available_positions(piece.color,
+                                                       to_row=move_to_row,
+                                                       from_row=move_from_row,
+                                                       to_col=move_to_col,
+                                                       from_col=move_from_col)
+        return result
+
+    def capture_piece(self, piece_to_move, move_to_row, move_to_col):
+        if piece_to_move.color == "w":
+            self.white_captured.append(self._board[move_to_row][move_to_col])
         else:
-            print("This position already has a piece in")
+            self.black_captured.append(self._board[move_to_row][move_to_col])
 
-
+    def move_piece(self, piece_to_move, move_to_row, move_to_col, move_from_row, move_from_col):
+        new_pos = self._board[move_to_row][move_to_col]
+        able_to_move = self.check_available_moves(piece_to_move, move_to_row, move_to_col, move_from_row, move_from_col)
+        if new_pos is " ":
+            if able_to_move:
+                self._board[move_to_row][move_to_col] = piece_to_move
+                self._board[move_from_row][move_from_col] = " "
+            else:
+                raise Exception("This movement can not be done.")
+        elif new_pos != " " and new_pos.color is not piece_to_move.color:
+            if able_to_move:
+                self.capture_piece(piece_to_move, move_to_row, move_to_col)
+                self._board[move_to_row][move_to_col] = piece_to_move
+                self._board[move_from_row][move_from_col] = " "
+            else:
+                raise Exception("This movement can not be done.")
+        else:
+            raise Exception("This position already has a piece of your color in")
 
 
 b = Board()
 b.print_board()
 
 turn_count = 0
-turn = "White"
+turn = "w"
 
 
 while True:
     b.print_board()
     print(f"Turn: #{turn_count} {turn}\n")
-    print(f"From which position do you wish to move (row col): ")
+    print(f"Which piece do you wish to move (row col): ")
     from_row = input()
     from_col = input()
-    piece = b.check_piece(int(from_row), int(from_col))
-    print(f"Which position do you want to move your piece (row col): ")
-    to_row = input()
-    to_col = input()
-    b.check_position(piece, int(to_row), int(to_col), int(from_row), int(from_col))
-    turn = "White" if turn == "Black" else "Black"
-    turn_count += 1
-    input()
+    try:
+        piece = b.check_piece(int(from_row), int(from_col))
+        print(f"Where do you want to move your piece (row col): ")
+        to_row = input()
+        to_col = input()
+
+        b.move_piece(piece, int(to_row), int(to_col), int(from_row), int(from_col))
+        turn = "w" if turn == "b" else "b"
+        turn_count += 1
+    except Exception as e:
+        print(f"\033[91m{e}\033[00m")
+        print(f"Click enter to choose again.")
+        input()
+        continue
